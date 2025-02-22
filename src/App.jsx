15cc7c9ffd46840c33af4e536fe2b78111a82da2
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-02-22 09:39:51 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <div className="container bg-red-500 mx-auto">
+        <h1 className="text-5xl">Project Management System</h1>
+      </div>
+    </div>
+  )
+}
+export default App
